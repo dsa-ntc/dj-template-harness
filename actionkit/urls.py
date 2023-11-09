c4fe9e5a73c4f1641a746302d725e79af6b50015
Ty Schlichenmeyer <ty.schlich@gmail.com>
2023-11-09 04:38:58 +0000

    add actionkit view/url

--- actionkit/urls.py
+++ actionkit/urls.py
@@ -1,5 +1,1 @@
-from django.shortcuts import render
-
-
-def recurring_update(request):
-    return render(request, 'recurring_update.html')
+urlpatterns = []
---
+++ actionkit/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+
+def recurring_update(request):
+    return render(request, 'recurring_update.html')
